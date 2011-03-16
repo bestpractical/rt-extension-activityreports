e19fa72b96d6e97a786a8f6786fb9975961d4f2f
afresh1 <andrew+github@afresh1.com>
2011-03-15 18:33:08 -0700

    Make Start and End dates work properly, or at least how I expect Allow either a single ticket or an RT::Tickets object and return an RT::Transactions object for all relevent tickets

--- lib/RT/Extension/ActivityReports.pm
+++ lib/RT/Extension/ActivityReports.pm
@@ -1,11 +1,14 @@
 package RT::Extension::ActivityReports;
+use RT::Date;
 
 use Exporter qw( import );
 @EXPORT_OK = qw( RelevantTxns );
 
-our $VERSION = '1.01';
+our $VERSION = '1.02';
 
 =head2 RelevantTxns( $ticket, \%args )
+
+$ticket can either be a single RT::Ticket or an RT::Tickets object.
 
 Helper routine for the various activity reports, to get the list of
 relevant transactions on each relevant ticket.  Not yet used for
@@ -21,12 +24,30 @@ Resolution* or TimeWorked reports.  Args include:
 sub RelevantTxns {
     my( $ticket, %args ) = @_;
 
-    my $txns = $ticket->Transactions;
-    $txns->Limit(FIELD => 'Created', OPERATOR => '>=', VALUE => $args{start});
-    $txns->Limit(FIELD => 'Created', OPERATOR => '<=', VALUE => $args{end});
+    my $txns = new RT::Transactions($ticket->CurrentUser);
+    if ( $ticket->isa('RT::Tickets') ) {
+        while (my $t = $ticket->Next) {
+            $txns->LimitToTicket($t->id);
+        }
+    } else {
+        $txns = $ticket->Transactions;
+    }
+
+    my $date = new RT::Date( $ticket->CurrentUser );
+
+    $date->Set( Format => 'unknown', Value => $args{start} );
+    my $start = $date->AsString( Format => 'sql' );
+
+    $date->Set( Format => 'unknown', Value => $args{end} );
+    $date->AddSeconds(86399) if $date->AsString =~ /\b00:00:00\b/;
+    my $end = $date->AsString( Format => 'sql' );
+
+
+    $txns->Limit(FIELD => 'Created', OPERATOR => '>=', VALUE => $start, ENTRYAGGREGATOR => 'AND');
+    $txns->Limit(FIELD => 'Created', OPERATOR => '<=', VALUE => $end, ENTRYAGGREGATOR => 'AND');
     if( $args{timed} ) {
 	# Limit to transactions with positive time taken.
-        $txns->Limit(FIELD => 'TimeTaken', OPERATOR => '>=', VALUE => 1); 
+        $txns->Limit(FIELD => 'TimeTaken', OPERATOR => '>=', VALUE => 1, ENTRYAGGREGATOR => 'AND'); 
     } else {
 	# Include status changes and ticket creations.
 	$txns->Limit(FIELD => 'Type', VALUE => 'Status', ENTRYAGGREGATOR => 'OR');
