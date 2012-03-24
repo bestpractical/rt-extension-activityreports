3b9a12a777302e0473b5e7ab93c9bdd06ab043c8
Kevin Falcone <falcone@bestpractical.com>
2012-03-23 23:02:24 -0400

    release the dev release as 1.02

--- lib/RT/Extension/ActivityReports.pm
+++ lib/RT/Extension/ActivityReports.pm
@@ -3,7 +3,7 @@ package RT::Extension::ActivityReports;
 use Exporter qw( import );
 @EXPORT_OK = qw( RelevantTxns );
 
-our $VERSION = '1.01_01';
+our $VERSION = '1.02';
 
 =head2 RelevantTxns( $ticket, \%args )
 
