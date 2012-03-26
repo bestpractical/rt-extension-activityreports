afd68e3a6649e4cb3a322f7f80b12e5cdffa5eff
Thomas Sibley <trs@bestpractical.com>
2012-03-26 11:21:09 -0400

    Bump version and doc changes

--- lib/RT/Extension/ActivityReports.pm
+++ lib/RT/Extension/ActivityReports.pm
@@ -3,7 +3,7 @@ package RT::Extension::ActivityReports;
 use Exporter qw( import );
 @EXPORT_OK = qw( RelevantTxns );
 
-our $VERSION = '1.02';
+our $VERSION = '1.03';
 
 =head2 RelevantTxns( $ticket, \%args )
 
