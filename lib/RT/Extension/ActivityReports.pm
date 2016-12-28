ab40bd9fbb3c7643b7d234e22924df9a312bc53e
Kailas Narendran <kailas@xitome.com>
2016-12-28 16:06:38 -0500

    Updated RelevantTxns to use AND aggregator on start and end to only get transactions in the start-end window
    Added TimeWorked as a displayed output and updated the ticket # to link to the transaction itself

--- lib/RT/Extension/ActivityReports.pm
+++ lib/RT/Extension/ActivityReports.pm
@@ -132,19 +132,21 @@ sub RelevantTxns {
     my( $ticket, %args ) = @_;
 
     my $txns = $ticket->Transactions;
-    $txns->Limit(FIELD => 'Created', OPERATOR => '>=', VALUE => $args{start});
-    $txns->Limit(FIELD => 'Created', OPERATOR => '<=', VALUE => $args{end});
+    $txns->Limit(FIELD => 'Created', OPERATOR => '>=', VALUE => $args{start},ENTRYAGGREGATOR => 'AND');
+    $txns->Limit(FIELD => 'Created', OPERATOR => '<=', VALUE => $args{end},ENTRYAGGREGATOR => 'AND');
     if( $args{timed} ) {
-	# Limit to transactions with positive time taken.
+    # Limit to transactions with positive time taken.
         $txns->Limit(FIELD => 'TimeTaken', OPERATOR => '>=', VALUE => 1); 
     } else {
-	# Include status changes and ticket creations.
-	$txns->Limit(FIELD => 'Type', VALUE => 'Status', ENTRYAGGREGATOR => 'OR');
-	$txns->Limit(FIELD => 'Type', VALUE => 'Create', ENTRYAGGREGATOR => 'OR');
+    # Include status changes and ticket creations.
+    $txns->Limit(FIELD => 'Type', VALUE => 'Status', ENTRYAGGREGATOR => 'OR');
+    $txns->Limit(FIELD => 'Type', VALUE => 'Create', ENTRYAGGREGATOR => 'OR');
     }
     # Comment/correspond type transactions are always relevant.
     $txns->Limit(FIELD => 'Type', VALUE => 'Comment', ENTRYAGGREGATOR => 'OR');
+    $txns->Limit(FIELD => 'Type', VALUE => 'Set', ENTRYAGGREGATOR => 'OR');
     $txns->Limit(FIELD => 'Type', VALUE => 'Correspond');
+
 
     return $txns;
 }
