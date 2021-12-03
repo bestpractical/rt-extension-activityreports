c20e60f04d80c1d12788d0054edf097b39525dff
Jim Brandt <jbrandt@bestpractical.com>
2021-12-03 11:01:43 -0500

    Prep version 1.09

--- lib/RT/Extension/ActivityReports.pm
+++ lib/RT/Extension/ActivityReports.pm
@@ -3,7 +3,7 @@ package RT::Extension::ActivityReports;
 use Exporter qw( import );
 @EXPORT_OK = qw( RelevantTxns );
 
-our $VERSION = '1.08';
+our $VERSION = '1.09';
 
 RT->AddStyleSheets('activity-reports.css');
 
